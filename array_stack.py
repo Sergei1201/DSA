d4bac7af332e61a9241e2fc03e83864d166aa152
Sergei Sokolov <sokolov12ssa@gmail.com>
2025-01-13 21:39:45 +0500

    next commit

--- array_stack.py
+++ array_stack.py
@@ -1,14 +1,15 @@
-# Implementing stack data structure in Python using lists
+# Stack data structure in Python using lists
 
-# Strategy
-# 1. Create an empty stack
+# Strategy 
+# 1. Create an empty stack using lists
 # 2. Check if the stack is empty
 # 3. Push an element onto the stack
-# 4. Pop the top element from the stack
-# 5. Print the stack
+# 4. Pop an element from the top of the stack
+# 5. Pring the stack
+
 
 # Create an empty stack
-def create_stack() ->list:
+def create_stack() -> list:
     stack = []
     return stack
 
@@ -20,39 +21,40 @@ def is_empty(stack: list) -> bool:
         return False
 
 # Push an element onto the stack
-def push_element(stack: list, element: int) -> None: 
-        stack.append(element)
-        print(f"The element pushed: {element}")
+def push_element(stack: list, element: int) -> None:
+    stack.append(element)
+    print(f"The pushed element: {element}")
 
-# Pop an element from the top of the stack
+
+# Pop from the top of the stack
 def pop_element(stack: list) -> None:
     # Check if the stack is empty
     if (is_empty(stack)):
         print(f"The stack is empty")
         return
     else:
-        # Otherwise pop the top element from the stack
-        print(f"The popped element {stack.pop()}")
+        print(f"The poppped element: {stack.pop()}")
 
 # Print the stack
-def print_stack(stack: list) -> None:
-    print("The stack:", end=' ')
+def print_stack(stack) -> None:
+    print("The stack: ", end=' ')
     for element in stack:
-        print(element, end=' ')
+        print(f"{element}", end=' ')
+
     print()
 
 if __name__ == '__main__':
     # Create an empty stack
     stack = create_stack()
     # Push elements
-    push_element(stack, 5)
     push_element(stack, 10)
+    push_element(stack, 20)
+    push_element(stack, 30)
+    # Print the stack
     print_stack(stack)
     pop_element(stack)
     pop_element(stack)
     pop_element(stack)
+    pop_element(stack)
     print_stack(stack)
-
-    
-
 
