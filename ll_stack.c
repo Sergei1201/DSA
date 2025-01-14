1c9ead543bae47cf70ed55173ad15c9bfbd3d330
Sergei Sokolov <sokolov12ssa@gmail.com>
2025-01-14 21:30:06 +0500

    ll_stack_second_version

--- ll_stack.c
+++ ll_stack.c
@@ -1,122 +1,147 @@
-/* Stack implementation in C using a single linked list */ 
+/* Implementing the stack data structure using linked list in C */ 
 
 /* Strategy */ 
-/* 1. Create a structure for a node
-   2. Create a linked list itself
-   3. Create a new node
-   4. Check if the linked list is empty
-   5. Push a new node onto the stack
-   6. Remove the top node from the stack
-   7. Peek at the top node of the stack
-   8. Print elements of the stack
-  */
+/* 1. Define node using struct 
+   2. Define stack 
+   3. Initialize stack
+   4. Check if the stack is empty
+   5. Create a new node on the stack
+   6. Push a new node onto the stack
+   7. Pop the top node from the stack
+   8. Peek at the top node of the stack
+   9. Print the stack
+*/ 
+
+#include <endian.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-/* Function definition */ 
-
-// Create a structure for a linked list
+// Node data structure 
 typedef struct Node {
 	int data;
 	struct Node *next;
 } Node;
 
-// Stack
+// Define stack
 typedef struct Stack {
 	Node *head;
 } Stack;
 
-// Initialize a new stack
+// Initialize the stack (the default stack will be empty which means the head will point to null
 void initialize_stack(Stack *stack) {
 	stack->head = NULL;
-}	
+}
+
+// Check if the stack is empty
+bool is_empty(Stack *stack) {
+	return stack->head == NULL;
+}
+
 // Create a new node
 Node* create_new_node(int data) {
-	// Dynamically allocate memory on the heap for a new node
+
+	// Memory allocation on the heap for a new node
 	Node *new_node = (Node *)malloc(sizeof(Node));
 	new_node->data = data;
 	return new_node;
 }
 
-bool is_empty(Stack *stack) {
-	return stack->head == NULL;
-}
-
-// Push a new node
+// Push a new node onto the stack
 void push_node(Stack *stack, int data) {
-	// Push a new node onto the stack
+
+	// Create new node by calling function defined above
 	Node *new_node = create_new_node(data);
-	// Check if the memory allocation for the new node failed
+
+	// Make sure the memory has bee allocated properl and no mistakes have occurred during this process
 	if (!new_node) {
-		printf("Stack overflow");
+		printf("Memory allocation error");
 		return;
-	}
-	// Link the new node to the head
+	}	
+
+	// Connect the newly created node to the head
 	new_node->next = stack->head;
-	// Shift the head of the linked list to the newly created node
-	stack->head = new_node;
+
+       // Rearrange the pointer to the new head (new node)
+       stack->head = new_node;
+       
+       // Print the new node added at the top of the stack
+       printf("Pushed %d\n", stack->head->data );
 }
 
-// Pop the top node from the linked list
+// Pop the top node from the stack
 void pop_node(Stack *stack) {
+
 	// Check if the stack is empty
 	if (is_empty(stack)) {
-		printf("Stack is empty\n");
+		printf("The stack is empty\n");
 		return;
 	} else {
 		// Create a temporary node pointing to the head
 		Node *temp = stack->head;
-	        // Shift the head to the next node
-	        stack->head = stack->head->next;
-       	        // Free the memory for the temp node
-	        free(temp);	       
-	}
-} 
 
-// Peek at the top node of the stack
-void peek_node(Stack *stack) {
-	// Check if the stack is empty
-	if (is_empty(stack)) {
-		printf("The stack is empty");
-		return;
-	} else {
-		printf("The top node: %d\n", stack->head->data);
+		// Shift the head to the next node
+		stack->head = stack->head->next;
+
+		// Print the node we're popping off of the stack
+		printf("Popped %d\t\n", temp->data);
+		
+		// Free memory occupied by the temp node
+		free(temp);
 	}
 }
 
-// Print the stack
-void print_stack(Stack *stack) {
-	Node *temp = stack->head;
-	printf("The stack: ");
-	while (temp) {
-		printf("%d\t", temp->data);
-		temp = temp->next;
+// Peek at the top element of the stack
+void peek_element(Stack *stack) {
+
+	// Check if the stack is empty
+	if (is_empty(stack)) {
+		printf("The stack is empty\n");
+		return;
+	} else {
+		printf("The top element %d\t\n", stack->head->data);
 	}
+}
+
+// Print elements of the stack
+void print_stack(Stack *stack) {
+
+	// Create a temp node to traverse through the linked list and print data on each iteration
+	// starting from the top of the stack
+	printf("Stack: ");
+	Node *temp = stack->head;
+	while (temp) {
+		printf("%d\t", temp->data );
+		temp = temp->next;
+	}	
 	printf("\n");
 }
 
+/* Main function */
 int main(void) {
-	Stack stack;
-	initialize_stack(&stack);
-	push_node(&stack, 10);
-	push_node(&stack, 20);
-	push_node(&stack, 30);
-	push_node(&stack, 40);
-	push_node(&stack, 50);
-	print_stack(&stack);
-	peek_node(&stack);
-	pop_node(&stack);
-	pop_node(&stack);
-	print_stack(&stack);
-	peek_node(&stack);
-	pop_node(&stack);
-	pop_node(&stack);
-	print_stack(&stack);
-	peek_node(&stack);
-	pop_node(&stack);
-	print_stack(&stack);
-	pop_node(&stack);
 
-	return 0;
+// Create a new stack dynamically on the heap at runtime
+Stack *st = (Stack *)malloc(sizeof(Stack));
+
+// Initialize the stack
+initialize_stack(st);
+// Push onto the stack
+push_node(st, 10);
+push_node(st, 20);
+push_node(st, 30);
+push_node(st, 40);
+push_node(st, 50);
+print_stack(st);
+peek_element(st);
+pop_node(st);
+pop_node(st);
+pop_node(st);
+pop_node(st);
+pop_node(st);
+pop_node(st);
+print_stack(st);
+free(st);
+return 0;
+
 }
+
